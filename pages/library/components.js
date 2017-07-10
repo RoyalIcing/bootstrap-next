38908908c054ef4cfdf4bcd251653b9e2da3b5cd
Patrick Smith <patrick@burntcaramel.com>
2017-07-10 19:12:20 +1000

    Support nested components on library page

--- pages/library/components.js
+++ pages/library/components.js
@@ -3,31 +3,50 @@ import js from 'react-syntax-highlighter/dist/languages/javascript'
 import theme from 'react-syntax-highlighter/dist/styles/hopscotch'
 
 import { files, componentFiles } from '../../api/listComponents'
-import Main from '../../components/Main'
+import Main from '../../organisms/Main'
 import Row from '../../components/Row'
 import Col from '../../components/Col'
+import PrimaryNav from '../../molecules/PrimaryNav'
 
 registerLanguage('javascript', js)
+
+const ComponentFile = ({
+  fileName,
+  contents
+}) => (
+  <Row>
+    <Col>
+      {
+        (typeof contents === typeof '') ? (
+          <div>
+            <h2>{ fileName }</h2>
+            <SyntaxHighlighter
+              language='javascript'
+              style={ theme }
+              
+              children={ contents }
+            />
+          </div>
+        ) : (
+          Object.keys(contents).map(fileName => (
+            <ComponentFile fileName={ fileName } contents={ contents[fileName ] } />
+          ))
+        )
+      }
+    </Col>
+  </Row>
+)
 
 
 export default ({
 
 }) => (
   <Main title='Library: Components'>
+    <PrimaryNav />
     <h1>Components Library</h1>
   {
     Object.keys(componentFiles).map(fileName => (
-      <Row>
-        <Col>
-          <h2>{ fileName }</h2>
-          <SyntaxHighlighter
-            language='javascript'
-            style={ theme }
-            
-            children={ componentFiles[fileName] }
-          />
-        </Col>
-      </Row>
+      <ComponentFile fileName={ fileName } contents={ componentFiles[fileName ] } />
     ))
   }
   </Main>
