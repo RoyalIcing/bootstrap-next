bb8244bf2ab8dc0e587d3a0f1fbce116a57e62f9
Patrick Smith <patrick@burntcaramel.com>
2017-07-11 13:25:24 +1000

    Remove unused code

--- api/readFilesIn.js
+++ api/readFilesIn.js
@@ -7,11 +7,9 @@ function readFilesIn(dirPath, subpath = './') {
   return files.reduce((namesToContents, fileName) => {
     const filePath = Path.join(dirPath, subpath, fileName)
     const stats = FS.statSync(filePath)
-    //const key = Path.join.apply(Path, subpath.concat(fileName))
     const key = Path.join(subpath, fileName)
     if (stats.isDirectory()) {
       namesToContents[key] = readFilesIn(dirPath, Path.join(subpath, fileName))
-      //namesToContents[key] = FS.readdirSync(Path.join(dirPath, subpath, fileName)).join('') //FS.readdirSync(Path.join(dirPath, subpath, fileName)).join(', ')
     }
     else if (stats.isFile()) {
       const content = FS.readFileSync(filePath).toString()
